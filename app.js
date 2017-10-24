b139c51de93c53f6fdbe5a39eb89b54d923c095a
Jarven <769558579@qq.com>
2017-10-24 11:19:36 +0800

    commit

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log('getUserInfoP')
               console.log(res.userInfo)
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,3 +1,4 @@
+const app = getApp()
 // pages/my/my.js
 Page({
 
@@ -5,14 +6,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      userInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      userInfo:app.globalData.userInfo
+    })
   },
 
   /**

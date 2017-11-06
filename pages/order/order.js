73393d8c1cc38b4173fb245480b0bc7efc1427d7
Cao yang <caoyang@CaodeMacBook-Pro.local>
2017-11-06 17:57:43 +0800

    添加訂單列表

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,6 +1,5 @@
 // pages/order/order.js
 var Zan = require('../../component/zanui-weapp/dist/tab/index');
-
 Page(Object.assign({}, Zan, {
   data: {
     tab: {
@@ -22,26 +21,144 @@ Page(Object.assign({}, Zan, {
       }],
       selectedId: 'all',
       scroll: false,
-      fixed:true
+      fixed: true
     },
-    selectType:''//当前目录
+    goods: [],
+    selectType: '',//当前目录
+    goodsTest: '',
   },
   handleZanTabChange(e) {
     var componentId = e.componentId;
     var selectedId = e.selectedId;
     //控制选中样式
     this.setData({
-      [componentId+`.selectedId`]: selectedId
+      [componentId + `.selectedId`]: selectedId
     });
+    if (selectedId == "topay") {
+      this.setData({
+        goods: [
+          {
+            goodId: "2031",
+            goodsImg: "https://img11.360buyimg.com/n5/jfs/t11401/34/504898878/183378/5a6a8a48/59f1c3a1N2174c8cc.jpg",
+            goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+            goodsSttus: "待付款",
+            price: "50",
+            buttonTitle: "去付款"
+          }
+        ],
+      }
+      )
+    }
+    if (selectedId == "all") {
+      this.setData({
+        goods: [
+          {
+            goodId: "2030",
+            goodsImg: "https://img10.360buyimg.com/n5/jfs/t3394/97/927974603/289198/faa13081/5816f2e5Nab3d62be.jpg",
+            goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+            goodsSttus: "已完成",
+            price: "40",
+            buttonTitle: "再次购买"
+          }, {
+            goodId: "2031",
+            goodsImg: "https://img11.360buyimg.com/n5/jfs/t11401/34/504898878/183378/5a6a8a48/59f1c3a1N2174c8cc.jpg",
+            goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+            goodsSttus: "待付款",
+            price: "50",
+            buttonTitle: "去付款"
+          }, {
+            goodId: "2032",
+            goodsImg: "https://img11.360buyimg.com/n5/jfs/t9853/275/2215551794/187986/425288fd/59f1c3a2Ne3576868.jpg",
+            goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+            goodsSttus: "待发货",
+            price: "60",
+            buttonTitle: "再次购买"
+          }, {
+            goodId: "2033",
+            goodsImg: "https://img10.360buyimg.com/n5/jfs/t3394/97/927974603/289198/faa13081/5816f2e5Nab3d62be.jpg",
+            goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+            goodsSttus: "待收货",
+            price: "70",
+            buttonTitle: "确认收货"
+          },
+        ]
+      })
+    }
   },
+  /**
+   * 去付款
+   */
+  goodsAction(e) {
+    var dataItem = e.currentTarget.dataset;
+    console.log(dataItem.item)
+    if (dataItem.item.buttonTitle == "去付款") {
+      wx.navigateTo({
+        url: '/pages/pay/pay?id=1',
+      })
+    }
+  },
+  getDataFromNet() {
+    this.setData({
+      goods: [
+        {
+          goodId: "2030",
+          goodsImg: "https://img10.360buyimg.com/n5/jfs/t3394/97/927974603/289198/faa13081/5816f2e5Nab3d62be.jpg",
+          goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+          goodsSttus: "已完成",
+          price: "40",
+          buttonTitle: "再次购买"
+        }, {
+          goodId: "2031",
+          goodsImg: "https://img11.360buyimg.com/n5/jfs/t11401/34/504898878/183378/5a6a8a48/59f1c3a1N2174c8cc.jpg",
+          goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+          goodsSttus: "待付款",
+          price: "50",
+          buttonTitle: "去付款"
+        }, {
+          goodId: "2032",
+          goodsImg: "https://img11.360buyimg.com/n5/jfs/t9853/275/2215551794/187986/425288fd/59f1c3a2Ne3576868.jpg",
+          goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+          goodsSttus: "待发货",
+          price: "60",
+          buttonTitle: "再次购买"
+        }, {
+          goodId: "2033",
+          goodsImg: "https://img10.360buyimg.com/n5/jfs/t3394/97/927974603/289198/faa13081/5816f2e5Nab3d62be.jpg",
+          goodsStr: "清风(APP)抽纸 原木纯品金装系列3层130抽*16包纸巾(整箱销售)",
+          goodsSttus: "待收货",
+          price: "70",
+          buttonTitle: "确认收货"
+        },
+      ]
+    })
 
+
+    // let params = {},
+    // wx.request({
+    //   url: '',
+    // })
+    // wx.request({
+    // url: '',
+    // method: 'GET',
+    // data: params,
+    // success: function (res) {
+    //   console.log(res);
+    //   this.setData({
+    //     deliveryId: res.data.data.id
+    //   });
+    //   wx.navigateBack({ delta: 1 });
+    // },
+    // fail: function (err) {
+    //   console.log(err);
+    // }
+    // })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("options");
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -53,7 +170,7 @@ Page(Object.assign({}, Zan, {
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getDataFromNet()
   },
 
   /**
@@ -74,7 +191,7 @@ Page(Object.assign({}, Zan, {
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -5,19 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    customerOpt:{
-      isMessage:true,
-      word:''
+    customerOpt: {
+      isMessage: true,
+      word: ''
     },
-    showTokenDialog:false,//token优惠券Dialog开关
-    deliveryFlag:true
+    btnTitle: "提交订单",
+    showTokenDialog: false,//token优惠券Dialog开关
+    deliveryFlag: true,
   },
   /**
    * 获取数据
    */
-  getData(e){
+  getData(e) {
     let obj = {
-      customerOpt:{}
+      customerOpt: {}
     }
     obj.customerOpt[e.currentTarget.dataset.name] = e.detail.value;
     this.setData(obj);
@@ -26,7 +27,7 @@ Page({
   /**
    * 页面跳转
    */
-  skipToPage(e){
+  skipToPage(e) {
     console.log(e);
     let url = e.currentTarget.dataset.url;
     wx.navigateTo({
@@ -36,7 +37,7 @@ Page({
   /**
    * 打开token列表
    */
-  toggleDialog(){
+  toggleDialog() {
     this.setData({
       showTokenDialog: !this.data.showTokenDialog
     })
@@ -44,13 +45,41 @@ Page({
   /**
    * 是否发送短信
    */
-  isSendMessage(e){
+  isSendMessage(e) {
     this.setData({
-      customerOpt:{
-        isMessage:e.detail.value
+      customerOpt: {
+        isMessage: e.detail.value
       }
     })
   },
+  /**
+   * 下单
+   */
+  gotoPay() {
+     this.setData({
+       btnTitle:"去支付",
+     })
+     wx.showLoading({
+       title: '加载中',
+     })
+     this.readyToPay()
+  },
+  /**
+   * 准备支付
+   */
+  readyToPay:function()
+  {
+    console.log("去支付!!!")
+    // wx.hideLoading()
+    // wx.requestPayment({
+    //   timeStamp: '',
+    //   nonceStr: '',
+    //   package: '',
+    //   signType: '',
+    //   paySign: '',
+    // })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,48 +91,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    btnTitle: "立即下单"
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
